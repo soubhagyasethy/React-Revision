51020560cd72aead2414e560cf347bdcb66b6844
soubhagyasethy <ssethy017@gmail.com>
2024-06-18 13:00:42 +0530

    basic DOM minipulation using React

--- App.js
+++ App.js
@@ -1,4 +1,47 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello from React!";
-const root = document.getElementById("root");
-root.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello from React!";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "heading",
+    class: "class",
+    style: { color: "red" },
+  },
+  "Heading 1"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "heading",
+    class: "class",
+    style: { color: "red" },
+  },
+  "Heading 2"
+);
+
+const heading3 = React.createElement(
+  "h3",
+  {
+    id: "heading",
+    class: "class",
+    style: { color: "red" },
+  },
+  "Heading 3"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+    class: "container",
+    style: { backgroundColor: "yellow" },
+  },
+  [heading1, heading2, heading3]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
--- README.md
+++ README.md
@@ -12,3 +12,8 @@
 - Build your first Hello World program using,
 - Using just HTML
 - Using JS to manipulate the DOM
+- Using React
+- use CDN Links
+- Create an Element
+- Create nested React Elements
+- Use root.render
